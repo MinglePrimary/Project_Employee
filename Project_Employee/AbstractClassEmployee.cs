b357cfbcc59f98cd29cf74c110cc8e411fb21b70
= <=>
2019-10-27 21:13:27 +0700

    try first commit

---
+++ Project_Employee/AbstractClassEmployee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Employee
+{
+    public abstract class Employee
+    {
+
+        private string firstName;
+        private string lastName;
+        // constructor
+        public Employee(string firstNameValue, string lastNameValue)
+        {
+            firstName = firstNameValue;
+            lastName = lastNameValue;
+        }
+
+        //properties
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+        public abstract decimal Earnings();
+    }
+}
---
+++ Project_Employee/HourlyWorkerClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Employee
+{
+    class HourlyWorker: Employee 
+    {
+        private decimal wage;
+        private double hoursWorked;
+
+        // constructor
+        public HourlyWorker(string firstNameValue, string LastNameValue,
+                            decimal wageValue, double hoursWorkedValue)
+            :base( firstNameValue, LastNameValue )
+        {
+            wage = wageValue;
+            HoursWorked = hoursWorkedValue;
+        }
+
+        public decimal Wage
+        {
+            get { return wage; }
+            set
+            {
+                if (value > 0)
+                    wage = value;
+            }
+        }
+        public double HoursWorked
+        {
+            get { return hoursWorked; }
+            set
+            {
+                if (value > 0)
+                    hoursWorked = value;
+            }
+        }
+        public override decimal Earnings()
+        {
+            if (HoursWorked <= 40)
+            {
+                return Wage * Convert.ToDecimal(HoursWorked);
+            }
+            else
+            {
+                decimal basePay = Wage * Convert.ToDecimal(40);
+                decimal overtimPay = Wage * 1.5M * Convert.ToDecimal(HoursWorked - 40);
+                return basePay + overtimPay;
+            }
+        }
+        public override string ToString()
+        {
+            return "HourlyWorker: " + base.ToString();
+        }
+    }
+}
